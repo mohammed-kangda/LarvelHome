fde2001788c2ee93999bbd78006538e5f2851781
mohammed-kangda <kangdamohammed551@gmail>
2022-08-09 17:32:34 +0530

    Initial Commit

--- laravelJetstream/routes/web.php
+++ laravelJetstream/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,12 +18,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+
+Route::group(['prefix' => 'admin', 'middleware' => ['admin:admin']],function(){
+    Route::get('/login',[AdminController::class,'loginForm']);
+    Route::post('/login',[AdminController::class,'store'])->name('admin.store');
 });
+
+
+Route::middleware(['auth:sanctum,admin','verified'])->get('/admin/dashboard',function(){
+    return view('dashboard');
+})->name('dashboard');
+
+
+Route::middleware(['auth:sanctum,web','verified'])->get('/dashboard',function(){
+    return view('dashboard');
+})->name('dashboard');
+
+
+
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
